7edd57a92f9bd3109d60f8aaf3cb86fce65885fe
simo <simosoftware01@gmail.com>
2024-09-03 16:32:43 +0300

    Finished Hero.jsx

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { assets } from '../assets/frontend_assets/assets';
 function Hero() {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
@@ -8,8 +9,15 @@ function Hero() {
                 <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
                 <p className="font-medium text-sm md:text-base uppercase">Our Bestsellers</p>
             </div>
+            <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">Latest Arrivals</h1>
+            <div className="flex items-center gap-2">
+              <p className="font-semibold text-sm md:text-base uppercase">shop</p>
+              <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            </div>
         </div>
         </div>
+        {/* {Hero Right Side} */}
+        <img className='w-full sm:w-1/2' src= {assets.hero_img} alt="" />
     </div>
   )
 }
