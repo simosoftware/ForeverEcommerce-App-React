8b898a6766f46c978316aeedd27072d439422a66
simo <simosoftware01@gmail.com>
2024-09-04 14:55:13 +0300

    Best Seller Component

--- Frontend/src/components/Title.jsx
+++ Frontend/src/components/Title.jsx
@@ -1,3 +1,4 @@
+
 const Title = ({ text1, text2 }) => {
   return (
     <div className="inline-flex gap-2 items-center mb-3">
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import BestSeller from '../components/BestSeller';
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Hero />
       <LatestCollection/>
+      <BestSeller/>
     </div>
   );
 };
