ada0022be8ef3ecbbe13a7e26c6ec6d4a78f683f
simo <simosoftware01@gmail.com>
2024-11-01 16:20:26 +0300

    Cart jsx

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/frontend_assets/assets';
+import { toast } from 'react-toastify';
 
 export const ShopContext = createContext();
 
@@ -12,14 +13,21 @@ const ShopContextProvider = (props) => {
 
   const addToCart = async (itemId, size) => {
 
+    if(!size){
+      toast.error("Select Product Size")
+      return
+    }
+
     let cartData = structuredClone(cartItems);
 
     if (cartData[itemId]) {
+
       if (cartData[itemId][size]) {
         cartData[itemId][size] += 1;
       } else {
         cartData[itemId][size] = 1;
       }
+
     } else {
       cartData[itemId] = {};
       cartData[itemId][size] = 1;
@@ -27,6 +35,27 @@ const ShopContextProvider = (props) => {
 
     setCartItems(cartData);
   };
+
+  const getCartCount = () => {
+    let totalCount  = 0;
+    for(const items in cartItems){
+      for(const item in cartItems[items]){
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];            
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalCount;
+  }
+
+  // useEffect(()=>{
+  //   console.log(cartItems);
+    
+  // },[cartItems])
 
 
   const value = {
@@ -37,7 +66,9 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
-    cartItems
+    cartItems,
+    addToCart,
+    getCartCount
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,5 +1,61 @@
+import { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={'YOUR'} text2={' CART'} />
+      </div>
+      <div className="pt-2">
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  src={productData.image[0]}
+                  alt={productData.name}
+                  className="w-16 h-20"
+                />
+              <div className="">
+                <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                <div className="flex text-center">
+                  <p>{currency}{productData.price}</p>
+                </div>
+              </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
